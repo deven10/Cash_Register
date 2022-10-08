571a65ee03181b7460e8e745129cd07f02a40aa8
Deven Umrania <32352481+deven10@users.noreply.github.com>
2022-10-09 00:05:48 +0530

    Not solved

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 const billAmount = document.querySelector("#bill-amount");
-const checkButton = document.querySelector("#check-button");
 const cashGiven = document.querySelector("#cash-given");
-const errorMessage = document.querySelector("#error-message");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".notes");
+const availableNotes = [2000, 500, 100, 50, 20, 5, 1];
 
-checkButton.addEventListener("click", () => {
-    errorMessage.style.display = "none";
-    if(billAmount.value > 0){
+checkButton.addEventListener("click", function validate(){
+    hideMessage();
+     if(billAmount.value > 0){
         if(cashGiven.value >= billAmount.value){
-
-        }else {
-            showMessage("The cash provided is incorrect");
+            let amountToReturn = cashGiven.value - billAmount.value;
+            calculate(amountToReturn);
+        } else{
+            showMessage("The cash provided is less than your bill");
         }
     } else {
         showMessage("Invalid Bill Amount");
     }
 });
 
-function showMessage(message){
-    errorMessage.style.display = "block";
-    errorMessage.innerHTML = message;
+function calculate(amountToReturn){
+    for(let i = 0; i < availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(amountToReturn / availableNotes[i]);
+        amountToReturn %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+function hideMessage(){
+    message.style.display = "none";
+}
+
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerHTML = msg;
 }
